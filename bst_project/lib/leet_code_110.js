1a1e3579f78a5ef27985ba340e3cfd97df48aee0
Henry Chu <chu.henry@hotmail.com>
2019-04-13 19:34:17 -0700

    BST project leetcode question

--- bst_project/lib/leet_code_110.js
+++ bst_project/lib/leet_code_110.js
@@ -3,5 +3,17 @@
 
 
 function isBalanced(root) {
+  return getHeight(root) !== -1;
 
+  function getHeight(root) {
+    if (!root) {
+      return 0;
+    }
+    let left = getHeight(root.left);
+    let right = getHeight(root.right);
+    if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+      return -1;
+    }
+    return 1 + Math.max(left, right);
+  }  
 }
